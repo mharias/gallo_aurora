50cff424abd5aaa3dd4ce15b1acc54d3d578c27c
mharias <mharias@me.com>
2024-05-26 20:19:39 +0200

    version vm nueva

--- gallo_cloud_new.py
+++ gallo_cloud_new.py
@@ -12,7 +12,7 @@ import pandas as pd
 import sys
 import tweepy
 
-sys.path.append("/Users/mharias/documents/proyectos/mylibs") # directorio de acceso a librerías auxiliares
+#sys.path.append("/Users/mharias/documents/proyectos/mylibs") # directorio de acceso a librerías auxiliares
 sys.path.append("/home/waly/claves") # directorio de acceso a librerías auxiliares
 
 from pass_elgallo import token_elgalloaurora,apikey,apisecretkey,AccessToken,AccessTokenSecret,google_key
